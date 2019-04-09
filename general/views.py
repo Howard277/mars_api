0d731989c80162dee25ad8337a327a069417009b
wuketao <wuketao@houbank.com>
2019-04-09 14:14:58 +0800

    调试模型

--- general/views.py
+++ general/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import User
+from django.core import serializers
+import json
+from django.http import HttpResponse, HttpRequest
+
 
 # Create your views here.
+def login(request):
+    login_name = request.POST['loginname']
+    password = request.POST['password']
+    if (login_name is not None) and (password is not None):
+        userList = User.objects.filter(login_name=login_name, password=password)
+        if userList.count() > 0:
+            user = userList[0]  # type:User
+            return HttpResponse(json.loads('true'),
+                                content_type="application/json")
+    return HttpResponse(json.loads('false'),
+                        content_type="application/json")
--- mars_api/models.py
+++ mars_api/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class GeneralModel(models.Model):
     create_login_name = models.CharField(max_length=50)  # 创建人登录名
     create_time = models.DateTimeField(auto_now=True)  # 创建时间
+
+    class Meta:
+        abstract = True
