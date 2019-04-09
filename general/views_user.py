928228eb3cc4da9947559f9968d279c1cd311eca
wuketao <wuketao@houbank.com>
2019-04-09 18:24:53 +0800

    优化

--- general/views_user.py
+++ general/views_user.py
@@ -1,13 +1,14 @@
 from .models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 import json
 from mars_api.tools import queryset_to_dict
 
 
 # 获取分页用户信息
 def get_user_page(request):
-    pageIndex = request.POST.get('pageIndex', 0)
-    pageSize = request.POST.get('pageSize', 10)
+    post = request.POST  # type:QueryDict
+    pageIndex = post.get('pageIndex', 0)
+    pageSize = post.get('pageSize', 10)
     all = User.objects.all()
     count = all.count()
     userList = all[pageIndex * pageSize:(pageIndex + 1) * pageSize]
