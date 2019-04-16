019af5307e0495143061ddd8f256c1f2fab2a803
wuketao <wuketao@houbank.com>
2019-04-16 14:57:41 +0800

    优化登录功能

--- general/views.py
+++ general/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
 from .models import User
-from django.core import serializers
 import json
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, QueryDict
+from mars_api.tools import model_to_dict
 
 
 # Create your views here.
+# 登录接口
 def login(request):
-    login_name = request.POST['loginname']
-    password = request.POST['password']
-    if (login_name is not None) and (password is not None):
+    body = request.body  # type:bytes
+    params = json.loads(body.decode(encoding='utf-8'))
+    login_name = params.get('loginName', '')
+    password = params.get('password', '')
+    data = {'status': False}
+    if (len(login_name) > 0) and (len(password) > 0):
         user_list = User.objects.filter(login_name=login_name, password=password)
         if user_list.count() > 0:
             user = user_list[0]  # type:User
-            return HttpResponse(json.loads('true'),
-                                content_type="application/json")
-    return HttpResponse(json.loads('false'),
+            data = {'status': True, 'userInfo': model_to_dict(user), 'menus': [{
+                'index': 'mainpage',
+                'name': '首页',
+                'path': '/'
+            }, {
+                'index': 'customermanagement',
+                'name': '客户管理'
+            }, {
+                'index': 'customerlist',
+                'name': '客户列表',
+                'path': '/customerlist'
+            }, {
+                'index': 'config',
+                'name': '配置管理',
+                'path': '/config'
+            }]}
+    return HttpResponse(json.dumps(data),
                         content_type="application/json")
